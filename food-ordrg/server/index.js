cedd30d4ee284ec4c35940e4660db62b03e0520a
aziz <khardaniaziz@gmail.com>
2021-07-15 14:18:21 +0100

    structure

--- food-ordrg/server/index.js
+++ food-ordrg/server/index.js
@@ -1,1 +1,14 @@
-const aziz=()=>{}
+const express = require('express');
+// const db = require('./db');
+const app = express();
+const port = 3000;
+
+
+app.use(express.json())
+app.use(express.static(__dirname + "/../client/build"));
+app.use(express.urlencoded({ extended: true }));
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
